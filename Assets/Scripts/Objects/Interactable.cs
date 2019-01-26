5c41f616d45a24f0ee31761f9d8be0bff67a9abc
Tobias Bruch <tobias.bruch@gmx.de>
2019-01-26 21:38:48 +0100

    Add outline to interactable objects

--- Assets/Scripts/Objects/Interactable.cs
+++ Assets/Scripts/Objects/Interactable.cs
@@ -11,6 +11,19 @@ public class Interactable : MonoBehaviour
     [SerializeField]
     public UnityAction _onCarryClicked = delegate { };
 
+    private QuickOutline _outline;
+
+    protected virtual void Awake()
+    {
+        _outline = gameObject.AddComponent<QuickOutline>();
+
+        _outline.OutlineMode = QuickOutline.Mode.OutlineAll;
+        _outline.OutlineColor = Color.white;
+        _outline.OutlineWidth = 7f;
+
+        _outline.enabled = false;
+    }
+
     public virtual void OnCarryClick()
     {
         _onCarryClicked.Invoke();
@@ -23,11 +36,13 @@ public class Interactable : MonoBehaviour
 
     public void OnPointerEnter()
     {
+        _outline.enabled = true;
 
     }
 
     public void OnPointerExit()
     {
+        _outline.enabled = false;
 
     }
 }
