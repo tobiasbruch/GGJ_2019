49ed570559e54cb1a13de1560bc5e1f2ae35088c
phil <phil.zupke@gmail.com>
2019-01-26 18:17:02 +0100

    putting doors in our home

---
+++ Assets/Scripts/Objects/OpenDoor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : Interactable
+{
+    public GameObject Collider;
+    public Animator DoorAnimator;
+    public override void OnInteractClick()
+    {
+        base.OnInteractClick();
+        DoorAnimator.SetBool("openDoor", true);
+        Invoke("DeactivateCollider", 1f);
+    }
+
+    void DeactivateCollider()
+    {
+        Collider.SetActive(false);
+    }
+}
