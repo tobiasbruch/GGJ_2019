6c6b28aa8f5b4f600851f71d3694ab1b93b8c055
Eyal Stern <kokostern@gmail.com>
2019-01-26 17:52:56 +0100

    - fader code and audio source pool

---
+++ Assets/Scripts/Audio/AudioSourcePool.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+    public class AudioSourcePool
+    {
+        private List<AudioSource> _audioSources;
+
+        private GameObject _gameObject;
+
+        public AudioSourcePool(GameObject gameObject)
+        {
+            _gameObject = gameObject;
+            _audioSources = new List<AudioSource>();
+        }
+
+        public AudioSource GetSoundSource()
+        {
+            AudioSource source = null;
+
+            for (int i = 0; i < _audioSources.Count; i++)
+            {
+                if (!_audioSources[i].isPlaying)
+                {
+                    source = _audioSources[i];
+                    break;
+                }
+            }
+
+            if (source != null)
+            {
+                return source;
+            }
+
+            source = CreateNewAudioSource();
+            _audioSources.Add(source);
+            return source;
+        }
+
+        private AudioSource CreateNewAudioSource()
+        {
+            return _gameObject.AddComponent<AudioSource>();
+        }
+    }
+
