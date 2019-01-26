9b3262f98781a04f17e23256ea150342777065aa
Tobias Bruch <tobias.bruch@gmx.de>
2019-01-26 12:20:07 +0100

    Add interactable objects

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _crosshairAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirstPersonRaycaster.Instance.EventPointerEnterInteractable += OnPointerEnterInteractable;
+        FirstPersonRaycaster.Instance.EventPointerExitInteractable += OnPointerExitInteractable;
+    }
+
+    private void OnPointerEnterInteractable(Interactable interactable)
+    {
+        _crosshairAnimator.SetBool("PointerOverInteractable", true);
+    }
+
+    private void OnPointerExitInteractable(Interactable interactable)
+    {
+        _crosshairAnimator.SetBool("PointerOverInteractable", false);
+    }
+}
---
+++ Assets/Scripts/FirstPersonRaycaster.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class FirstPersonRaycaster : MonoBehaviour
+{
+    public static FirstPersonRaycaster Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        } else
+        {
+            Debug.LogError("Cant have multiple instances of FirstPersonRaycaster");
+            Destroy(gameObject);
+        }
+    }
+
+    private Interactable _currentInteractable;
+
+    public event Action<Interactable> EventPointerEnterInteractable = delegate { };
+    public event Action<Interactable> EventPointerExitInteractable = delegate { };
+    public event Action<Interactable> EventInteractInteractable = delegate { };
+    public event Action<Interactable> EventCarryInteractable = delegate { };
+
+    private void Update()
+    {
+        ProcessInput();
+    }
+    private void FixedUpdate()
+    {
+        Raycast();
+    }
+
+    private void ProcessInput()
+    {
+        if (_currentInteractable)
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                EventInteractInteractable(_currentInteractable);
+                _currentInteractable.OnInteractClick();
+            }
+            if (Input.GetButtonDown("Carry"))
+            {
+                EventCarryInteractable(_currentInteractable);
+                _currentInteractable.OnCarryClick();
+            }
+        }
+    }
+
+    private void Raycast()
+    {
+        RaycastHit hit = new RaycastHit();
+        Ray ray = new Ray(transform.position, transform.forward);
+        if(Physics.Raycast(ray, out hit))
+        {
+            var interactable = hit.collider.GetComponent<Interactable>();
+            if (interactable)
+            {
+                if (interactable != _currentInteractable)
+                {
+                    if(_currentInteractable != null)
+                    {
+
+                        EventPointerExitInteractable(_currentInteractable);
+                        _currentInteractable.OnPointerExit();
+                        _currentInteractable = null;
+                    }
+
+                    _currentInteractable = interactable;
+                    EventPointerEnterInteractable(_currentInteractable);
+                    _currentInteractable.OnPointerEnter();
+                }
+            } else
+            {
+                if (_currentInteractable != null)
+                {
+                    EventPointerExitInteractable(_currentInteractable);
+                    _currentInteractable.OnPointerExit();
+                    _currentInteractable = null;
+                }
+            }
+        }
+    }
+}
