7d744e34fb22c7796f1475c79f76ede02d9e2012
phil <phil.zupke@gmail.com>
2019-01-27 13:25:45 +0100

    we now have toilet paper in our home Added sounds to doors

--- Assets/Scripts/LightSwitch.cs
+++ Assets/Scripts/LightSwitch.cs
@@ -6,11 +6,15 @@ public class LightSwitch : Interactable
 {
     public Light Light;
     public AudioSource Audio;
-
+    public AudioClip OffSound;
+    public AudioClip OnSound;
     public override void OnInteractClick()
     {
         base.OnInteractClick();
+
+        Audio.PlayOneShot(Light.enabled ? OffSound : OnSound);
         Light.enabled = !Light.enabled;
-        Audio.Play();
+
+     
     }
 }
