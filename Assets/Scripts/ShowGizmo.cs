2c3f0936144d5573a36e25caf1bedb0f6e4ce8c0
phil <phil.zupke@gmail.com>
2019-01-27 01:21:29 +0100

    everything in our home looks better and is more fun now

---
+++ Assets/Scripts/ShowGizmo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowGizmo : MonoBehaviour
+{
+    public Color Color;
+    public GizmoTypes Type;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color;
+        switch (Type)
+        {
+            case GizmoTypes.cube:
+                Gizmos.DrawWireCube(transform.position, transform.localScale);
+                break;
+            case GizmoTypes.sphere:
+                Gizmos.DrawWireSphere(transform.position, transform.localScale.magnitude);
+                break;
+        }
+  
+    }
+
+    public enum GizmoTypes
+    {
+        cube, sphere
+    }
+}
