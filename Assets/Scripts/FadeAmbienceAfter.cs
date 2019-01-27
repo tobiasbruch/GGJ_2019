dde03940996a58c8a1b0869f77bec51aabe0b2bd
Eyal Stern <kokostern@gmail.com>
2019-01-27 14:55:52 +0100

    - Ambience goes away after some time

---
+++ Assets/Scripts/FadeAmbienceAfter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FadeAmbienceAfter : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        StartCoroutine(StopAfter());
+    }
+
+    private IEnumerator StopAfter()
+    {
+        yield return new WaitForSeconds(15);
+        _audioSource.Stop();
+        
+    }
+}
